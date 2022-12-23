06b7e929f963a613b4be3410149b8446353f0410
luhundun <57153251@qq.com>
2022-12-23 15:54:10 +0800

    修改

--- src/config/req.ts
+++ src/config/req.ts
@@ -1,5 +1,8 @@
 export const  pyURL:string =  'https://c2788c96-4ad3-4755-8392-218181168a83.mock.pstmn.io/'
-// export const  pyURL:string =  'https://c2788c96-4ad3-4755-8392-218181168a83.mock.pstmn.io/'
 export const  baseURL:string = 'http://127.0.0.1:8080'
-
 export const  timeout:number = 16000
+
+
+// export const  pyURL:string =  'https://localhost:8000'
+// export const  baseURL:string = 'http://127.0.0.1:8080'
+// export const  timeout:number = 160000000
